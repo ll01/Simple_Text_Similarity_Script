bc69d31d62514c63ff2a74f9cc2d3a3bed6bf701
David Mcpherson <david.mcpherson@argos.co.uk>
2020-01-09 23:52:02 +0000

    ...

--- clean.py
+++ clean.py
@@ -1,4 +1,29 @@
 import tensorflow as tf
 import main
+import csv
+import html
+import re
 
-main.readCsv("./")
+def remove_html_tags(text):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
+
+def remove_no_description(text):
+    clean = re.compile('.*,,.*\n')
+    return re.sub(clean, '', text)
+
+def remove_no_title(text):
+    clean = re.compile(',\n')
+    return re.sub(clean, '', text)
+
+
+with open("./argosProduct2020-01-09.csv", "r") as data_file:
+    with open("./argos.csv","w" ) as clean_file:
+        data = data_file.read()
+        data = html.unescape(data)
+        data = remove_no_description(data)
+        data = remove_no_title(data)
+        clean_file.write(remove_html_tags(data))
+
+
+
