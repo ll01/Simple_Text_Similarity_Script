f455b8e71955fac622a73dd82bce43abb4535391
David Mcpherson <david.mcpherson@argos.co.uk>
2020-01-09 14:09:11 +0000

    multi embedings

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import tensorflow as tf
 import tensorflow_hub as hub
+import sentencepiece as spm
+import bert
+import os
+import csv
+import numpy as np
 
 
 def main():
-    texts = [
-        "my name is David",
-        "David is the name I was given"
-    ]
-    print("similar texts")
-    how_similar_USE(texts[0], texts[1])
+    header, argos_items_data = readCsv("./MOCK_DATA_small.csv")
+    print(argos_items_data)
+    test_sainsbury_item = ["sainsburys sku", "title", "description"]
 
-    texts_not_similar = [
-        "Boats are really cool",
-        "Dude where are we ?"
-    ]
-    print("not similar texts")
-    how_similar_USE(texts_not_similar[0], texts_not_similar[1])
+    USE(test_sainsbury_item, argos_items_data)
+    
 
+    # ALBERT(argos, sainsbury)
 
-def encode_using_universal_sentence_encoder(text, model_from_hub):
-    embeddings = model_from_hub([text])
-    embeddings_normalized = tf.math.l2_normalize(embeddings)
-    return embeddings_normalized
-
+def readCsv(file_path):
+    data = []
+    with open(file_path,) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        header = None
+        for row in csvReader:
+            data.append(row)
+    header = data.pop(0)
+    return header, data
 
 def cosine_similarity(a, b):
 
@@ -35,17 +38,80 @@ def cosine_similarity(a, b):
     # normalizing between 1 and -1
     clip_cosine_similarities = tf.clip_by_value(cosine_similarity, -1.0, 1.0)
     return tf.acos(clip_cosine_similarities)
+    
 
 
 def get_similarity_score(a, b):
     return 1.0 - cosine_similarity(a, b)
 
 
-def how_similar_USE(first_text, second_text):
-    model = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-    a = encode_using_universal_sentence_encoder(first_text, model)
-    b = encode_using_universal_sentence_encoder(second_text, model)
-    print(get_similarity_score(a, b))
+class USE():
+    def __init__(self, original_text, comparison_items, batch_size=10000):
+        self.hub_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+        model = hub.load(self.hub_url)
+        a = self.encode_using_universal_sentence_encoder(original_text, model)
+        self.results = []
+        product_matrix = np.array(comparison_items)
+        product_matrix = tf.reshape(product_matrix, shape=(-1, batch_size))
+        
+        self.results = self.encode_using_universal_sentence_encoder(product_matrix, model)
+
+
+        # for text in comparison_texts:
+        #     b = self.encode_using_universal_sentence_encoder(text, model)
+        #     get_similarity_score(a, b)
+
+    def encode_using_universal_sentence_encoder(self, data, model_from_hub):
+        embeddings = model_from_hub(data)
+        embeddings_normalized = tf.math.l2_normalize(embeddings)
+        return embeddings_normalized
+
+
+class ALBERT():
+    def __init__(self, first_text, second_text):
+        model_name = "albert_base_v2"
+        model_dir = bert.fetch_google_albert_model(model_name, ".models")
+        model_ckpt = os.path.join(model_dir, "model.ckpt-best")
+        model_params = bert.albert_params(model_dir)
+
+        albert_layer = bert.BertModelLayer.from_params(
+            model_params, name="albert")
+        max_text_size = 128
+        self.model = self.build_network(albert_layer, max_text_size)
+        bert.load_albert_weights(albert_layer, model_ckpt)
+
+        spm_model = os.path.join(model_dir, "30k-clean.model")
+        sp = spm.SentencePieceProcessor()
+        sp.load(spm_model)
+
+        do_lower_case = True
+
+        token_ids_a = self.preprocess_input(
+            sp, first_text, do_lower_case)
+        token_ids_b= self.preprocess_input(
+            sp, second_text, do_lower_case)
+
+        text_prediction_a = self.predict_result(token_ids_a)
+        text_prediction_b = self.predict_result(token_ids_b)
+        print(get_similarity_score(text_prediction_a, text_prediction_b))
+
+    def build_network(self, albert_layer, max_text_size):
+        input_ids = tf.keras.layers.Input(
+            shape=(max_text_size,), dtype='int32')
+        output = albert_layer(input_ids)
+        model = tf.keras.Model(inputs=input_ids, outputs=output)
+        model.build(input_shape=(None, max_text_size))
+        return model
+
+    def preprocess_input(self, smp_model, text, lower=True):
+        processed_text =bert.albert_tokenization.preprocess_text(text, lower=lower)
+        return bert.albert_tokenization.encode_ids(smp_model, processed_text)
+# https://github.com/kpe#/bert-for-tf2
+
+    def predict_result(self, token_ids):
+        return self.model.predict(
+            token_ids, batch_size=1, use_multiprocessing=True)
+        
 
 
 if __name__ == "__main__":
---
+++ product.py
@@ -0,0 +1,5 @@
+class product():
+   def  __init__(self, sain__id, title, description) :
+        self.sain__id = sain__id
+        self.title = title
+        self.description = description
