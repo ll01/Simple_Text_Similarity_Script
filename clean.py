b91d821233fa48838d14350e7f6e5c0e2f2183cf
David Mcpherson <david.mcpherson@argos.co.uk>
2020-01-10 00:23:43 +0000

    cleaned data

--- clean.py
+++ clean.py
@@ -17,13 +17,17 @@ def remove_no_title(text):
     return re.sub(clean, '', text)
 
 
+
+
 with open("./argosProduct2020-01-09.csv", "r") as data_file:
-    with open("./argos.csv","w" ) as clean_file:
+    with open("./argos_reversed_title.csv","w" ) as clean_file:
         data = data_file.read()
         data = html.unescape(data)
         data = remove_no_description(data)
         data = remove_no_title(data)
+
         clean_file.write(remove_html_tags(data))
+# run  awk -F, '{print$1,$3,$2}' OFS=, argos_reversed_title.csv  > argos_swaped.csv after
 
 
 
