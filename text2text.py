5377e3535c0037ae8eadb79e67416a42fc8929c1
ll01 <enti2@hotmail.co.uk>
2020-05-22 11:44:45 +0100

    eddited to work on tf 2.1

--- text2text.py
+++ text2text.py
@@ -1,14 +1,18 @@
-import main
+import use
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU') 
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 def start():
-    # a = "hello my name is david"
-    # b = "welcome my name is david"
+    a = "hello my name is david"
+    b = "kevin is not very nice"
     
-    a = "Pure Siesta DAB/ FM Digital Alarm"
-    b = "Pure Siesta Mi Series 2 DAB/FM Alarm Clock Radio"
+    # a = "Pure Siesta DAB/ FM Digital Alarm"
+    # b = "Pure Siesta Mi Series 2 DAB/FM Alarm Clock Radio"
 
-    model = main.USE()
-    print (model.compare_texts(a,b))
+
+    use_model = use.USE()
+    print (use_model.compare_texts(a,b))
 
     start()
 if __name__ == "__main__":
